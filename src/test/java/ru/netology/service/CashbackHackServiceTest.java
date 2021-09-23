086be215b8a58fd1d705084973626022a5a486db
Call Me Hardcore <ik.veresk@gmail.com>
2021-09-23 13:28:41 +0300

    v3 part1

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,14 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldreturn1000()
+
+}
