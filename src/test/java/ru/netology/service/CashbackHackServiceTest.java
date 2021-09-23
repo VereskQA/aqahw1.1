d08bb49642003b1f4babb2dd238c27a9279d11ea
Call Me Hardcore <ik.veresk@gmail.com>
2021-09-23 13:37:29 +0300

    ver2 part2

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,6 +9,28 @@ public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void shouldreturn1000()
+    public void shouldReturn1000() {
+        assertEquals(1000, service.remain(0));
+    }
+
+    @Test
+    public void shouldReturn100() {
+        assertEquals(100, service.remain(900));
+    }
+
+    @Test
+    public void shouldReturn1() {
+        assertEquals(1, service.remain(999));
+    }
+
+    @Test
+    public void shouldReturn999() {
+        assertEquals(999, service.remain(1));
+    }
+
+    @Test
+    public void shouldReturn0() {
+        assertEquals(0, service.remain(1000));
+    }
 
 }
